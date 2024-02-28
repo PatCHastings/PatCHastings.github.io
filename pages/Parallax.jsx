8564deb75a128e0498c69b6597b39701bb1ab705
patCHastings <phastings82@gmail.com>
2024-02-28 16:00:18 -0500

    Modifications to css and parallax component

--- pages/Parallax.jsx
+++ pages/Parallax.jsx
@@ -17,12 +17,12 @@ const Parallax = () => {
   return (
     <div
       style={{
-        transform: `translateY(-${offsetY * 0.3}px)`,
+        transform: `translateY(-${offsetY * 1}px)`,
         height: "100vh", // Adjust height as needed
         background: "url(/rolling-hills.jpg) no-repeat center center fixed",
         backgroundSize: "cover",
         position: "absolute",
-        height: "2000px",
+        //height: "500px",
         top: 0,
         left: 0,
         right: 0,
